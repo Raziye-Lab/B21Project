db3fc967f03dafee5b6496cb1aa2ac650b6413ce
Raziye-Lab <reziya709@gmail.com>
2020-11-10 12:49:47 -0500

    added more words

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println(" B21");
+        System.out.println("It's been a good day");
     }
 }
